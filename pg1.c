20c0557d2642da726cfec8581681cacc72672353
anushahonwad <anushahonwad@gmail.com>
2025-09-16 15:11:21 +0530

    3 line

--- pg1.c
+++ pg1.c
@@ -1,4 +1,8 @@
 this is my first file in my project1
 this is my second line
 this line from feature branch
+<<<<<<< HEAD
 this line from feature branch1
+=======
+this is my 3lines
+>>>>>>> feature_branch
